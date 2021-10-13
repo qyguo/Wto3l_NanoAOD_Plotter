cfd278635558d3b7fbe32baf7aef0dadad3fb0d1
Nik Menendez <nikmenendez@ufl.edu>
2021-10-13 16:06:02 -0400

    Now saves efficiency table to output folder

--- make_plots.py
+++ make_plots.py
@@ -27,9 +27,38 @@ if not os.path.exists("%s/pickle"%(out_dir)): os.makedirs("%s/pickle"%(out_dir))
 
 plots = [
 
+# 1D Plots
 #[Title,save name,variable plotted,nBins,low,high,unit,plot data]
 ["3 Mu Invariant Mass","m3l","m3l",83,0,83,"GeV",True],
+["3 Mu + MET Transverse Mass","mt","mt",200,0,200,"GeV",True],
 ["Lower Mass diMu Pair","mass2","M2",160,0,80,"GeV",False],
+["Higher Mass diMu Pair","mass1","M1",160,0,80,"GeV",False],
+["Leading pT","pTL1","pTL1",100,0,100,"GeV",True],
+["Subleading pT","pTL2","pTL2",100,0,100,"GeV",True],
+["Trailing pT","pTL3","pTL3",100,0,100,"GeV",True],
+["Leading eta","etaL1","etaL1",60,-3.,3.,"eta",True],
+["Subleading eta","etaL2","etaL2",60,-3.,3.,"eta",True],
+["Trailing eta","etaL3","etaL3",60,-3.,3.,"eta",True],
+["Leading phi","phiL1","phiL1",40,-4.,4.,"phi",True],
+["Subleading phi","phiL2","phiL2",40,-4.,4.,"phi",True],
+["Trailing phi","phiL3","phiL3",40,-4.,4.,"phi",True],
+["Leading Isolation","IsoL1","IsoL1",100,0.,0.2,"pfRelIso03_all",True],
+["Subleading Isolation","IsoL2","IsoL2",100,0.,0.2,"pfRelIso03_all",True],
+["Trailing Isolation","IsoL3","IsoL3",100,0.,0.2,"pfRelIso03_all",True],
+["Leading 3D Impact Parameter","ip3dL1","ip3dL1",100,0.,0.2,"IP3D",True],
+["Subleading 3D Impact Parameter","ip3dL2","ip3dL2",100,0.,0.2,"IP3D",True],
+["Trailing 3D Impact Parameter","ip3dL3","ip3dL3",100,0.,0.2,"IP3D",True],
+["Leading Significance of 3D Impact Parameter","sip3dL1","sip3dL1",100,0.,10.,"SIP3D",True],
+["Subleading Significance of 3D Impact Parameter","sip3dL2","sip3dL2",100,0.,10.,"SIP3D",True],
+["Trailing Significance of 3D Impact Parameter","sip3dL3","sip3dL3",100,0.,10.,"SIP3D",True],
+["Transverse Missing Energy","met","met",50,0,250,"GeV",True],
+["Transver Missing Energy Phi","met_phi","met_phi",40,-4,4,"phi",True],
+["dR Between Leading and Subleading","dR12","dR12",100,0,6,"dR",True],
+["dR Between Leading and Subleading","dR13","dR13",100,0,6,"dR",True],
+["dR Between Leading and Subleading","dR12","dR12",100,0,6,"dR",True],
+
+# 2D Plots
+#To be incorporated
 
 ]
 
@@ -84,6 +113,9 @@ for key in effs:
 	for key2 in effs[key]:
 		row.append("%.2f%%"%(effs[key][key2]))
 	x.add_row(row)
+table = open("%s/Efficiency_Table.txt"%(out_dir),"w")
+table.write(x.get_string())
+table.close()
 print(x)
 
 
