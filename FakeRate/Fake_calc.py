Fixed fake weight scaling and error propagation

FakeRate/Fake_calc.py
@@ -43,11 +43,12 @@ def fake_calc(data,s,nbins,out):
 		err_pass += er_pass
 		err_fail += er_fail
 
-	ratio1_b = y_fail/(y_pass+y_fail)
-	err1_b = ratio1_b*(err_fail/y_fail + (err_pass+err_fail)/(y_pass+y_fail))
+	ratio1_b = y_pass/(y_pass+y_fail)
+	err1_b = ratio1_b*(err_pass/y_pass + (err_pass+err_fail)/(y_pass+y_fail))
 
-	ratio2_b = y_pass/(y_fail-y_pass)
-	err2_b = ratio2_b*(err_pass/y_pass + (err_fail+err_pass)/(y_fail-y_pass))
+	ratio2_b = ratio1_b/(1-ratio1_b)
+	#err2_b = ratio2_b*(2*(err1_b/ratio1_b))
+	err2_b = ratio2_b*(err_pass/y_pass + err_fail/y_fail)
 
 	del y_pass, y_fail, err_pass, err_fail
 
@@ -91,11 +92,12 @@ def fake_calc(data,s,nbins,out):
 		err_pass += er_pass
 		err_fail += er_fail
 
-	ratio1_e = y_fail/(y_pass+y_fail)
-	err1_e = ratio1_e*(err_fail/y_fail + (err_pass+err_fail)/(y_pass+y_fail))
+	ratio1_e = y_pass/(y_pass+y_fail)
+	err1_e = ratio1_e*(err_pass/y_pass + (err_pass+err_fail)/(y_pass+y_fail))
 
-	ratio2_e = y_pass/(y_fail-y_pass)
-	err2_e = ratio2_e*(err_pass/y_pass + (err_fail+err_pass)/(y_fail-y_pass))
+	ratio2_e = ratio1_e/(1-ratio1_e)
+	#err2_e = ratio2_e*(2*(err1_e/ratio1_e))
+	err2_e = ratio2_e*(err_pass/y_pass + err_fail/y_fail)
 
 	errx = np.zeros(len(ratio1_e))
 	for i in range(len(ratio1_e)):
@@ -107,7 +109,7 @@ def fake_calc(data,s,nbins,out):
 	plt.ylim(bottom=0,top=1)
 	plt.xlim(left=nbins[0],right=nbins[-1])
 	plt.xlabel("Muon pT (GeV)")
-	plt.ylabel("fail/total")
+	plt.ylabel("pass/total")
 	plt.legend(loc='best')
 	plt.savefig("/home/nikmenendez/Output/%s/%s.png"%(out,"FakeRate_fail_total"))
 	plt.clf()
@@ -118,7 +120,7 @@ def fake_calc(data,s,nbins,out):
 	plt.ylim(bottom=0,top=1)
 	plt.xlim(left=nbins[0],right=nbins[-1])
 	plt.xlabel("Muon pT (GeV)")
-	plt.ylabel("pass/(fail-pass)")
+	plt.ylabel("f/(1-f)")
 	plt.legend(loc='best')
 	plt.savefig("/home/nikmenendez/Output/%s/%s.png"%(out,"FakeRate_pass_fail"))
 	plt.clf()

FakeRate/Fake_weight.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def Fake_weight(data,s,fwb,fwe,pt_bins):
 
-	if "fake" not in s:
-		return [1] * len(data["etaL3"])
+	#if "fake" not in s:
+	#	return [1] * len(data["etaL3"])
 
 	barrel = np.abs(data["etaL3"]) <= 1.4
 	endcap = np.abs(data["etaL3"]) >= 1.4

Plotter/Plot.py
@@ -27,6 +27,7 @@ def plot(data,p,s,e,out):
 	ax2 = fig.add_subplot(gs[7:,:])
 
 	last, data_made, MC_error, data_error = 0,[],0,0
+	tot_data, tot_MC, err_data, err_MC = 0, 0, 0, 0
 	for i in range(len(s)):
 		if len(p)<9:
 			to_plot = data[s[i]][p[2]][data[s[i]]["selection"]]
@@ -42,10 +43,35 @@ def plot(data,p,s,e,out):
 		hidden_error = np.sqrt(np.abs(y))*data[s[i]]["weight"]
 		if e or data[s[i]]["sType"]=="data":
 			error = hidden_error
+			tot_data = np.sum(weight_arr)
+			err_data = np.sqrt(tot_data)
+		elif data[s[i]]["sType"]=="MC":
+			error = 0
+			if not ("fake" in s[i]):
+				tot_MC += np.sum(weight_arr)
+				err_MC += np.sqrt(np.sum(weight_arr))
 		else:
 			error = 0
 		y,binEdges = np.histogram(to_plot,bins=p[3],range=(p[4],p[5]),weights=weight_arr)
 		bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
+
+		# Fake reweighting with WZ and ZZ
+		if "fake" in s[i]:
+			sam = "WZTo3LNu"
+			WZ_plot = data[sam][p[2]][data[sam]["fail"]]
+			WZ_weight = data[sam]["weight"]*data[sam]["genWeight"][data[sam]["fail"]]*data[sam]["pileupWeight"][data[sam]["fail"]]*data[sam]["fake_weight"][data[sam]["fail"]]
+			sam = "ZZTo4L"
+			ZZ_plot = data[sam][p[2]][data[sam]["fail"]]
+			ZZ_weight = data[sam]["weight"]*data[sam]["genWeight"][data[sam]["fail"]]*data[sam]["pileupWeight"][data[sam]["fail"]]*data[sam]["fake_weight"][data[sam]["fail"]]
+
+			yWZ, binEdges = np.histogram(WZ_plot,bins=p[3],range=(p[4],p[5]),weights=WZ_weight)
+			yZZ, binEdges = np.histogram(ZZ_plot,bins=p[3],range=(p[4],p[5]),weights=ZZ_weight)
+
+			y = y - yWZ - yZZ
+
+			tot_MC += np.sum(weight_arr) - np.sum(WZ_weight) - np.sum(ZZ_weight)
+			err_MC += np.sqrt(np.sum(weight_arr)) + np.sqrt(np.sum(WZ_weight)) + np.sqrt(np.sum(ZZ_weight))
+
 		if data[s[i]]["sType"]=="MC":
 			ax1.bar(bincenters,y,yerr=error,bottom=last,width=binEdges[1]-binEdges[0],label='%s: %.2f'%(s[i],np.sum(y)))
 			last += y
@@ -68,10 +94,10 @@ def plot(data,p,s,e,out):
 		ratioErr = ratio*(data_error/data_made + MC_error/last)
 		ax2.errorbar(bincenters,ratio,yerr=ratioErr,drawstyle='steps-mid',fmt="o",color='black')
 
-		tot_data = np.sum(data_made)
-		tot_MC = np.sum(last)
+		#tot_data = np.sum(data_made)
+		#tot_MC = np.sum(last)
 		dataMCratio = tot_data/tot_MC
-		dataMCerror = dataMCratio*(np.sum(MC_error)/tot_MC + np.sum(data_error)/tot_data)
+		dataMCerror = dataMCratio*(err_MC/tot_MC + err_data/tot_data)
 		ax1.text(0.0,1.0,'Data/Pred = %.2f +- %.2f'%(dataMCratio,dataMCerror),size=20,transform = ax1.transAxes)
 		
 

Skimmer/AnalysisSkimmer.py
@@ -24,14 +24,15 @@ def skim(data):
 	selection, eff["Subleading e pT > 15"] = cut((data["pTL2"]>15), selection)
 
 	selection, eff["No b Jets"]			= cut((data["nbJets"]==0), selection)
-	selection, eff["1 Good Muon"]		= cut((data["nGoodMuons"]==1), selection)
+	selection, eff[">=1 Good Muon"]		= cut((data["nGoodMuons"]>=1), selection)
 	selection, eff["medId True"]		= cut((data['medIdL3'] == 1), selection)
 	selection, eff["Isolation < 0.6"]	= cut((data["IsoL3"]<0.6), selection)
 	selection, eff["SIP3D < 4"]			= cut((data["sip3dL3"]<4), selection)
 
-	selection, eff["MET < 30"]          = cut((data["met"]<30), selection)
+	selection, eff["MET < 40"]          = cut((data["met"]<40), selection)
+	selection, eff["Only 3 leptons"]	= cut((data["nLeptons"]==3), selection)
 
-	selection, eff["M1 Around Z"]		= cut((data["M1"]>(91-15))&(data["M1"]<(91+15)),selection)
+	selection, eff["M1 Around Z"]		= cut((data["M1"]>(91-10))&(data["M1"]<(91+10)),selection)
 
 	if np.count_nonzero(selection)!=0:
 		eff["Overall"] = np.count_nonzero(selection)/len(data['nMuons'])*100
@@ -53,14 +54,15 @@ def skim_val(data):
 	selection, eff["Subleading e pT > 15"] = cut((data["pTL2"]>15), selection)
 
 	selection, eff["No b Jets"]			= cut((data["nbJets"]==0), selection)
-	selection, eff["1 Good Muon"]		= cut((data["nGoodMuons"]==1), selection)
+	selection, eff[">=1 Good Muon"]		= cut((data["nGoodMuons"]>=1), selection)
 	selection, eff["medId True"]		= cut((data['medIdL3'] == 1), selection)
 	selection, eff["Isolation < 0.6"]	= cut((data["IsoL3"]<0.6), selection)
 	selection, eff["SIP3D < 4"]			= cut((data["sip3dL3"]<4), selection)
 
-	selection, eff["MET < 30"]          = cut((data["met"]<30), selection)
+	selection, eff["MET < 40"]          = cut((data["met"]<40), selection)
+	selection, eff["Only 3 leptons"]    = cut((data["nLeptons"]==3), selection)
 
-	selection, eff["M1 Not Around Z"]	= cut(~((data["M1"]>(91-15))&(data["M1"]<(91+15))),selection)
+	selection, eff["M1 Not Around Z"]	= cut(~((data["M1"]>(91-10))&(data["M1"]<(91+10))),selection)
 
 	return selection, eff
 
@@ -70,6 +72,7 @@ def skim_flip(data,eff,s):
 	
 	selection = data['selection']
 	
+	fail = selection * data["IsoL3"]>iso
 	if "fake" in s:
 		selection, eff["Iso Cut"] = cut((data["IsoL3"]>iso), selection)
 	else:
@@ -80,4 +83,4 @@ def skim_flip(data,eff,s):
 	else:
 		eff["Overall"] = 0
 
-	return selection, eff
+	return selection, eff, fail